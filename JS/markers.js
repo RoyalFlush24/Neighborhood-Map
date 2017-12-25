f59ce79daf3ae56ead55f1aee9046e63d1c8781a
Ahmed Cherif <30884697+RoyalFlush24@users.noreply.github.com>
2017-12-25 12:58:35 -0600

    Add files via upload

---
+++ JS/markers.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'The MayFlower',
+        lat: 32.783226,
+        lng:  -96.800649,
+        type: 'Luxury Apartments'
+    },
+    {
+        title: 'Dakotas',
+        lat: 32.785055,
+        lng: -96.800664,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Convention Center Station',
+        lat: 32.773847,
+        lng: -96.803191,
+        type:  'Transit'
+    },
+    {
+        title: 'Galleria',
+        lat: 32.929946,
+        lng: -96.819835,
+        type:  'Shopping'
+    },
+    {
+        title: 'Meyerson Symphony Center',
+        lat: 32.789923,
+        lng: -96.798679,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Uptown Plaza',
+        lat: 32.793197,
+        lng: -96.802147,
+        type: 'Shopping'
+    },
+    {
+        title: 'Y.O. Ranch Steakhouse',
+        lat: 32.781293,
+        lng: -96.806399,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Iron Cactus Mexican Restaurant and Margarita Bar',
+        lat: 32.780537,
+        lng: -96.798621,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Cafe Momentum',
+        lat: 32.782055,
+        lng: -96.799465,
+        type: 'Restaurant'
+    },
+    {
+        title: 'West Village',
+        lat: 32.808298,
+        lng: -96.797343,
+        type: 'Shopping'
+    },
+    {
+        title: 'Texas Spice',
+        lat: 32.775212,
+        lng: -96.804280,
+        type: 'Restaurant'
+    }
+]
